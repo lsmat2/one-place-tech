5b32e4c60ee98516369c0c8bc66c72cdd01cd776
lsmat2 <Hockeybruh2>
2023-12-23 15:18:58 -0600

    Removed bubbles mess, separating background effects into separate file

--- one_place_tech.js
+++ one_place_tech.js
@@ -23,6 +23,43 @@
 // bubbleGif.addEventListener('click', changeSize("bubbleGif"));
 // document.getElementById("bubbleBackground").addEventListener('click', changeSize("bubbleBackground"));
 
+function createRandomDots(num) {
+    const numDots = num; // Number of dots to create
+  
+    for (let i = 0; i < numDots; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.style.top = Math.random() * document.body.clientHeight + 'px'; // 100 + 'vh'; // Random vertical position
+        dot.style.left = Math.random() * 100 + 'vw'; // Random horizontal position
+        document.body.appendChild(dot);
+    }
+}
+
+// console.log(document.body.clientHeight);
+// console.log(visualViewport.width);
+
+function changeDotProperties() {
+    const dots = document.querySelectorAll('.dot');
+  
+    dots.forEach(dot => {
+        // Lower dots each iteration at constant and random rates
+        const randMovementFactor = Math.floor(Math.random() * 5 + 1);
+        const constMovementFactor = 30;
+
+        const currentHeight = dot.getBoundingClientRect().top;
+        const newHeight = (currentHeight + constMovementFactor + randMovementFactor) % document.body.clientHeight;
+        const newHeightPix = newHeight + 'px';
+        dot.style.top = newHeightPix;
+    });
+    const height = dots[0].getBoundingClientRect().top;
+    const newHeightPix = (height + 1) + 'px';
+    dots[0].style.top = newHeightPix;
+    // console.log(newHeightPix);
+    // console.log(dots[0].style.top);
+}
+
+
+
 
 /*   <-------->     FUNCTIONALITY (ON CLICK)     <-------->   */
 
@@ -54,7 +91,7 @@ function changeSize(id, size) {
 function enlargeGif(id, size) {
     size.large = true;
     document.getElementById(id).style.height = '200px'; // if = '30vw', relative to 'Viewport Width'
-    document.getElementById("sortingTable").style.width = '90%';
+    // document.getElementById("sortingTable").style.width = '90%';
 };
 function reduceGif(id, size) {
     size.large = false;
@@ -62,3 +99,10 @@ function reduceGif(id, size) {
     document.getElementById(id).style.height = '60px';
     // document.getElementById("sortingTable").style.width = '70%';
 };
+
+function initialize() {
+    createRandomDots(30);
+    setInterval(changeDotProperties, 30);
+}
+
+initialize();
